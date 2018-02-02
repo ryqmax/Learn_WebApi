af423a6b79a4f83c2e4a6f6ff8246dd1247a6287
NiShiAn <30385099+NiShiAn@users.noreply.github.com>
2018-02-02 17:02:31 +0800

    WebApi文件下载

---
+++ RuntApi/Controllers/FileController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace RuntApi.Controllers
+{
+    /// <summary>
+    /// 文件Api
+    /// </summary>
+    [RoutePrefix("file")]
+    public class FileController : ApiController
+    {
+        /// <summary>
+        /// 文档下载
+        /// </summary>
+        [Route("download")]
+        [HttpGet]
+        public HttpResponseMessage DownLoadFile(string url, string fileName)
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(url) || string.IsNullOrEmpty(fileName))
+                    return new HttpResponseMessage(HttpStatusCode.NoContent);
+
+                var fileRespone = (HttpWebResponse) ((HttpWebRequest) WebRequest.Create(url)).GetResponse();
+                var response = new HttpResponseMessage(HttpStatusCode.OK);
+
+                byte[] buffer;
+                using (var stream = fileRespone.GetResponseStream())
+                {
+                    buffer = new byte[fileRespone.ContentLength];
+                    int offset = 0, actuallyRead;
+                    do
+                    {
+                        actuallyRead = stream?.Read(buffer, offset, buffer.Length - offset) ?? 0;
+                        offset += actuallyRead;
+                    }
+                    while (actuallyRead > 0);
+                }
+                response.Content = new StreamContent(new MemoryStream(buffer));
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+                {
+                    FileName = fileName
+                };
+                return response;
+            }
+            catch
+            {
+                return new HttpResponseMessage(HttpStatusCode.NoContent);
+            }
+        }
+
+    }
+}
